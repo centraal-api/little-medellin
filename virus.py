c2093b442ec29d7500ff10131a04f5ed88522920
German Eduardo Melo Acosta <german.melo@globant.com>
2020-04-05 11:28:29 -0500

    seveth commit

---
+++ virus.py
@@ -0,0 +1,13 @@
+from social import Person
+
+
+def infection (someone_a : Person, someone_b: Person, current_time: current_time):
+
+
+
+
+
+def remove(people: List[Person], current_time_event):
+
+
+
